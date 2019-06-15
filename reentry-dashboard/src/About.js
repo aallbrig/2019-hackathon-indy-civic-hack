b51968bf870c097a8c960eaea0610e76692bf993
aallbright <aallbrig@gmail.com>
2019-06-15 12:04:36 -0400

    Generate cards from data struct

--- reentry-dashboard/src/About.js
+++ reentry-dashboard/src/About.js
@@ -11,14 +11,22 @@ export default function About() {
   return (
     <div>
       <h2>About</h2>
+
+      <h4>Soteria</h4>
+
       <iframe
         title={uuidv4()}
         width="560"
         height="315"
         src="https://www.youtube.com/embed/PhqBx2A1hzY?start=5"
-        frameBorder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen />
+
+      <p>
+        Soteria, in Greek mythology, is the Goddess of salvation and safe passage. Our team selected her to represent
+        our team's solution because she best captures the values one should have when helping someone who has gone
+        through the criminal justice system.
+      </p>
     </div>
   );
 }
--- reentry-dashboard/src/Dashboard.js
+++ reentry-dashboard/src/Dashboard.js
@@ -6,127 +6,71 @@ import secondHelpings from './img/second-helpings.png';
 import callCenter from './img/call-center.png';
 import voiceAssistant from './img/voice-assistant.png';
 
+const PLACEHOLDER_IMAGE_SRC = 'https://via.placeholder.com/300x150';
+
+const cards = [
+  {
+    icon: <a href="/" target="_new"><Card.Img variant="top" className="custom-card-img" src={driversLicense} /></a>,
+    title: 'Licenses and Documentation help',
+    text: 'Example of overriding placeholder text'
+  },
+  {
+    icon: <a href="https://www.in.gov/judiciary/2625.htm?fbclid=IwAR1ad-QkZ_1dzN3qi4dZZ6Kh1yuNL6K11HdwlZOnxo036jSZGgbKXqUnwok" target="_new">
+      <Card.Img variant="top" className="custom-card-img" src={childSupport} />
+    </a>,
+    title: 'Child Support',
+  },
+  {
+    icon: <a href="https://www.secondhelpings.org/" target="_new">
+      <Card.Img variant="top" className="custom-card-img" src={secondHelpings}
+                alt="Second Helpings, transforming lives through the power of food" />
+    </a>,
+    title: 'Second Helpings'
+  },
+  {
+    icon: <a href="/" target="_new">
+      <Card.Img variant="top" className="custom-card-img" src={PLACEHOLDER_IMAGE_SRC} />
+    </a>,
+    title: 'Employer'
+  },
+  {
+    icon: <a href="/" target="_new">
+      <Card.Img variant="top" className="custom-card-img" src={callCenter} />
+    </a>,
+    title: 'Get Help'
+  },
+  {
+    icon: <a href="/" target="_new">
+      <Card.Img variant="top" className="custom-card-img" src={voiceAssistant} />
+    </a>,
+    title: 'Voice Assistance'
+  }
+];
+
+
 export default function Dashboard() {
-  const imageSrc = 'https://via.placeholder.com/300x150';
   return (
     <div>
       <h2>Dashboard</h2>
+
       <Row>
-        <Col>
-          <Card>
-            <a href="/" target="_new"><Card.Img variant="top" className="custom-card-img" src={driversLicense} /></a>
-            <Card.Body>
-              <center><Card.Title>Licenses and Documentation help</Card.Title></center>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <a href="/" target="_new">
-                <Button variant="primary" size="md" block>
-                  Go somewhere
-                </Button>
-              </a>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <a href="https://www.in.gov/judiciary/2625.htm?fbclid=IwAR1ad-QkZ_1dzN3qi4dZZ6Kh1yuNL6K11HdwlZOnxo036jSZGgbKXqUnwok" target="_new">
-              <Card.Img variant="top" className="custom-card-img" src={childSupport} />
-            </a>
-            <Card.Body>
-              <center><Card.Title>Child Support</Card.Title></center>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <a href="https://www.in.gov/judiciary/2625.htm?fbclid=IwAR1ad-QkZ_1dzN3qi4dZZ6Kh1yuNL6K11HdwlZOnxo036jSZGgbKXqUnwok" target="_new">
-                <Button variant="primary" size="md" block>
-                  Go somewhere
-                </Button>
-              </a>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <a href="https://www.secondhelpings.org/" target="_new">
-              <Card.Img variant="top" className="custom-card-img" src={secondHelpings} 
-              alt="Second Helpings, transforming lives through the power of food" />
-            </a>
-            <Card.Body>
-              <center><Card.Title>Second Helpings</Card.Title></center>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <a href="https://www.secondhelpings.org/" target="_new">
-                <Button variant="primary" size="md" block>
-                  Go somewhere
-                </Button>
-              </a>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Card>
-            <a href="/" target="_new">
-              <Card.Img variant="top" className="custom-card-img" src={imageSrc} />
-            </a>
-            <Card.Body>
-              <center><Card.Title>Employer</Card.Title></center>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <a href="/">
-                <Button variant="primary" size="md" block>
-                  Go somewhere
-                </Button>
-              </a>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <a href="/" target="_new">
-              <Card.Img variant="top" className="custom-card-img" src={callCenter} />
-            </a>
-            <Card.Body>
-              <center><Card.Title>Get Help</Card.Title></center>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <a href="/" target="_new">
-                <Button variant="primary" size="md" block>
-                  Go somewhere
-                </Button>
-              </a>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <a href="/" target="_new">
-              <Card.Img variant="top" className="custom-card-img" src={voiceAssistant} />
-            </a>
-            <Card.Body>
-              <center><Card.Title>Voice Assistance</Card.Title></center>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <a href="/">
-                <Button variant="primary" size="md" block>
-                  Go somewhere
-                </Button>
-              </a>
-            </Card.Body>
-          </Card>
-        </Col>
+        {cards.map(card => (
+          <Col xs={12} sm={4} md={3}>
+            <Card>
+              {card.icon ? card.icon : <Card.Img variant="top" className="custom-card-img" src={PLACEHOLDER_IMAGE_SRC} />}
+              <Card.Body>
+                <center>
+                  <Card.Title>
+                    {card.title ? card.title : 'Placeholder Title'}
+                  </Card.Title>
+                </center>
+                <Card.Text>
+                  {card.text ? card.text : 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
       </Row>
     </div>
   );
