7f29c4424e0b521244e4f94bae1ad128a71ad4b9
matthew.allbright <mattatttaq@gmail.com>
2019-06-15 11:33:33 -0400

    dashboard
    * add images
    * added some style to the images
    TODO:
    Working on links around images

--- reentry-dashboard/src/Dashboard.js
+++ reentry-dashboard/src/Dashboard.js
@@ -1,88 +1,66 @@
 import React from 'react';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Row, Col, Card, Button } from 'react-bootstrap';
+import planning from './img/planning.png';
+import driversLicense from './img/driving-license.png';
+import childSupport from './img/custody.png';
+import secondHelpings from './img/second-helpings.png';
+import callCenter from './img/call-center.png';
+import voiceAssistant from './img/voice-assistant.png';
 
 export default function Dashboard() {
+  const imageSrc = 'https://via.placeholder.com/300x150';
   return (
     <div>
       <h2>Dashboard</h2>
       <Row>
         <Col>
           <Card>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <a href="/"><Card.Img variant="top" className="custom-card-img" src={driversLicense} /></a>
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <center><Card.Title>Licenses and Documentation help</Card.Title></center>
               <Card.Text>
                 Some quick example text to build on the card title and make up the bulk of
                 the card's content.
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <a href="/">
+                <Button variant="primary" size="md" block>
+                  Go somewhere
+                </Button>
+              </a>
             </Card.Body>
           </Card>
         </Col>
         <Col>
           <Card>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <a href="/"><Card.Img variant="top" className="custom-card-img" src={childSupport} /></a>
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <center><Card.Title>Child Support</Card.Title></center>
               <Card.Text>
                 Some quick example text to build on the card title and make up the bulk of
                 the card's content.
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <a href="/">
+                <Button variant="primary" size="md" block>
+                  Go somewhere
+                </Button>
+              </a>
             </Card.Body>
           </Card>
         </Col>
         <Col>
           <Card>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" className="custom-card-img" src={secondHelpings} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <center><Card.Title>Second Helpings</Card.Title></center>
               <Card.Text>
                 Some quick example text to build on the card title and make up the bulk of
                 the card's content.
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <a href="/">
+                <Button variant="primary" size="md" block>
+                  Go somewhere
+                </Button>
+              </a>
             </Card.Body>
           </Card>
         </Col>
@@ -90,40 +68,52 @@ export default function Dashboard() {
       <Row>
         <Col>
           <Card>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" className="custom-card-img" src={imageSrc} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <center><Card.Title>Employer</Card.Title></center>
               <Card.Text>
                 Some quick example text to build on the card title and make up the bulk of
                 the card's content.
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <a href="/">
+                <Button variant="primary" size="md" block>
+                  Go somewhere
+                </Button>
+              </a>
             </Card.Body>
           </Card>
         </Col>
         <Col>
           <Card>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" className="custom-card-img" src={callCenter} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <center><Card.Title>Get Help</Card.Title></center>
               <Card.Text>
                 Some quick example text to build on the card title and make up the bulk of
                 the card's content.
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <a href="/">
+                <Button variant="primary" size="md" block>
+                  Go somewhere
+                </Button>
+              </a>
             </Card.Body>
           </Card>
         </Col>
         <Col>
           <Card>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" className="custom-card-img" src={voiceAssistant} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <center><Card.Title>Voice Assistance</Card.Title></center>
               <Card.Text>
                 Some quick example text to build on the card title and make up the bulk of
                 the card's content.
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <a href="/">
+                <Button variant="primary" size="md" block>
+                  Go somewhere
+                </Button>
+              </a>
             </Card.Body>
           </Card>
         </Col>
