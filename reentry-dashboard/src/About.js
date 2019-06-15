afaaf15e07973e8766677a7bd1c4f8816f59cade
matthew.allbright <mattatttaq@gmail.com>
2019-06-15 13:18:31 -0400

    Dashboard
    * media query for about
    * fixed navbar on mobile

--- reentry-dashboard/src/About.js
+++ reentry-dashboard/src/About.js
@@ -13,15 +13,15 @@ export default function About() {
       <h2>About</h2>
 
       <h4>Soteria</h4>
-
-      <iframe
-        title={uuidv4()}
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/PhqBx2A1hzY?start=5"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen />
-
+      <center>
+        <iframe
+          title={uuidv4()}
+          width="90%"
+          height="auto"
+          src="https://www.youtube.com/embed/PhqBx2A1hzY?start=5"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen />
+      </center>
       <p>
         Soteria, in Greek mythology, is the Goddess of salvation and safe passage. Our team selected her to represent
         our team's solution because she best captures the values one should have when helping someone who has gone
