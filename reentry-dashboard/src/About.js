497c5b31ae5df334b61948ab677f7268fc3fa5fe
aallbright <aallbrig@gmail.com>
2019-06-15 09:11:22 -0400

    Embed 'How to Pronounce Soteria' video

--- reentry-dashboard/src/About.js
+++ reentry-dashboard/src/About.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function About() {
-  return <h2>About</h2>;
+  return (
+    <div>
+      <h2>About</h2>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/PhqBx2A1hzY?start=5"
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen />
+    </div>
+  );
 }
