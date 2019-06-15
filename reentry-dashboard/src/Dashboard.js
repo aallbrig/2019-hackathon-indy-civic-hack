d0ba92316175577ba680116c0fa62ffc826fbd81
aallbright <aallbrig@gmail.com>
2019-06-15 12:23:52 -0400

    All cards are now the same height

--- reentry-dashboard/src/Dashboard.js
+++ reentry-dashboard/src/Dashboard.js
@@ -1,49 +1,44 @@
 import React from 'react';
-import { Row, Col, Card, Button } from 'react-bootstrap';
+import { Row, Col, Card } from 'react-bootstrap';
 import driversLicense from './img/driving-license.png';
 import childSupport from './img/custody.png';
 import secondHelpings from './img/second-helpings.png';
 import callCenter from './img/call-center.png';
 import voiceAssistant from './img/voice-assistant.png';
-
 const PLACEHOLDER_IMAGE_SRC = 'https://via.placeholder.com/300x150';
 
 const cards = [
   {
-    icon: <a href="/" target="_new"><Card.Img variant="top" className="custom-card-img" src={driversLicense} /></a>,
-    title: 'Licenses and Documentation help',
+    icon: <Card.Img variant='top' className='custom-card-img' src={voiceAssistant} />,
+    href: '/',
+    title: 'Get Help using Voice Assistance',
     text: 'Example of overriding placeholder text'
   },
   {
-    icon: <a href="https://www.in.gov/judiciary/2625.htm?fbclid=IwAR1ad-QkZ_1dzN3qi4dZZ6Kh1yuNL6K11HdwlZOnxo036jSZGgbKXqUnwok" target="_new">
-      <Card.Img variant="top" className="custom-card-img" src={childSupport} />
-    </a>,
-    title: 'Child Support',
+    icon: <Card.Img variant='top' className='custom-card-img' src={driversLicense} />,
+    href: '/',
+    title: 'Driver License'
   },
   {
-    icon: <a href="https://www.secondhelpings.org/" target="_new">
-      <Card.Img variant="top" className="custom-card-img" src={secondHelpings}
-                alt="Second Helpings, transforming lives through the power of food" />
-    </a>,
+    icon: <Card.Img variant='top' className='custom-card-img' src={childSupport} />,
+    href: 'https://www.in.gov/judiciary/2625.htm',
+    title: 'Child Support Calculator',
+  },
+  {
+    icon: <Card.Img variant='top' className='custom-card-img' src={secondHelpings}
+                alt='Second Helpings, transforming lives through the power of food' />,
+    href: 'https://www.secondhelpings.org/',
     title: 'Second Helpings'
   },
   {
-    icon: <a href="/" target="_new">
-      <Card.Img variant="top" className="custom-card-img" src={PLACEHOLDER_IMAGE_SRC} />
-    </a>,
+    icon: <Card.Img variant='top' className='custom-card-img' src={PLACEHOLDER_IMAGE_SRC} />,
+    href: '/',
     title: 'Employer'
   },
   {
-    icon: <a href="/" target="_new">
-      <Card.Img variant="top" className="custom-card-img" src={callCenter} />
-    </a>,
-    title: 'Get Help'
-  },
-  {
-    icon: <a href="/" target="_new">
-      <Card.Img variant="top" className="custom-card-img" src={voiceAssistant} />
-    </a>,
-    title: 'Voice Assistance'
+    icon: <Card.Img variant='top' className='custom-card-img' src={callCenter} />,
+    href: '/',
+    title: 'Call our Call Center'
   }
 ];
 
@@ -56,19 +51,21 @@ export default function Dashboard() {
       <Row>
         {cards.map(card => (
           <Col xs={12} sm={4} md={3}>
-            <Card>
-              {card.icon ? card.icon : <Card.Img variant="top" className="custom-card-img" src={PLACEHOLDER_IMAGE_SRC} />}
-              <Card.Body>
-                <center>
-                  <Card.Title>
-                    {card.title ? card.title : 'Placeholder Title'}
-                  </Card.Title>
-                </center>
-                <Card.Text>
-                  {card.text ? card.text : 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'}
-                </Card.Text>
-              </Card.Body>
-            </Card>
+            <a href={card.href} target='_new'>
+              <Card className={'h-100'}>
+                {card.icon ? card.icon : <Card.Img variant='top' className='custom-card-img' src={PLACEHOLDER_IMAGE_SRC} />}
+                <Card.Body>
+                  <center>
+                    <Card.Title>
+                      {card.title ? card.title : 'Placeholder Title'}
+                    </Card.Title>
+                  </center>
+                  <Card.Text>
+                    {card.text ? card.text : 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </a>
           </Col>
         ))}
       </Row>
